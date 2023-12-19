63d74e9db445555bbec496f041b1c726463e3b78
Matheus-Armando <matheuschioquetta1@gmail.com>
2023-12-19 08:38:05 -0300

    sidebarCrud

--- crudAngular/frontend/src/app/app.component.ts
+++ crudAngular/frontend/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/template/header/header.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent, MatToolbarModule, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, MatToolbarModule, FooterComponent, NavComponent],
   templateUrl: 'app.component.html',
   styles: [],
 })
